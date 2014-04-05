3df776ef1cac43b305c44ba019be99b1c1f3de20
Ruslan <ruslan_Maslouski@epam.com>
2014-04-05 20:38:55 +0300

    =

---
+++ raspisanie.grsu/CustomCell/ActiveLessonScheduleCell.h
@@ -0,0 +1,13 @@
+//
+//  ActiveLessonScheduleCell.h
+//  raspisanie.grsu
+//
+//  Created by Ruslan on 05.04.14.
+//  Copyright (c) 2014 RYSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActiveLessonScheduleCell : UITableViewCell
+
+@end
