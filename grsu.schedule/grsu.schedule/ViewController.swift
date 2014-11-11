e03a7d8d1ca603535052f99c780326212a52c8d3
Ruslan <Ruslan_Maslouski@epam.com>
2014-11-11 11:59:32 +0300

    init

---
+++ grsu.schedule/grsu.schedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  grsu.schedule
+//
+//  Created by Ruslan Maslouski on 11/11/14.
+//  Copyright (c) 2014 Ruslan Maslouski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
