f02394fa235d7320854baa41fa9fafac548c84f0
Ruslan Maslouski <zixzelz@gmail.com>
2014-12-25 20:00:27 +0300

    add google map sdk

---
+++ grsu.schedule/grsu.schedule/GUI/LessonLocationMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LessonLocationMapViewController.swift
+//  grsu.schedule
+//
+//  Created by Ruslan Maslouski on 12/25/14.
+//  Copyright (c) 2014 Ruslan Maslouski. All rights reserved.
+//
+
+import Cocoa
+
+class LessonLocationMapViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
