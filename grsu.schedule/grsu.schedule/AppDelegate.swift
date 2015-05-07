cd8c3a887da819fce7964b657b02747cef643d2e
Ruslan Maslouski <zixzelz@gmail.com>
2015-05-07 18:48:03 +0300

    release

--- grsu.schedule/grsu.schedule/AppDelegate.swift
+++ grsu.schedule/grsu.schedule/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyDRuBofs3NN-GSEwklztolgYNvcURPH80I")
+        GMSServices.provideAPIKey("AIzaSyBSF-hRXIjTMwnB0vwWcaDX-aq7WSy2pAc")
         
         Flurry.setCrashReportingEnabled(true)
         Flurry.startSession("9W5R9JWXFCGXR7XJ5R83")
