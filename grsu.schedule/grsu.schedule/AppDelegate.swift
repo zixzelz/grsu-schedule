19cd24a2bd336da1cdf13ba5559e667baf2494b7
Ruslan Maslouski <zixzelz@gmail.com>
2017-01-06 01:18:23 +0300

    added cleanCachejob

--- grsu.schedule/grsu.schedule/AppDelegate.swift
+++ grsu.schedule/grsu.schedule/AppDelegate.swift
@@ -24,24 +24,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         GSReachability.sharedInstance.startNotifier()
         cdh.setup()
+
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64( 3 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue(), { [weak self] _ in
+            self?.cleanCachejob()
+        })
+
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
+    func cleanCachejob() {
 
-    func applicationDidEnterBackground(application: UIApplication) {
-        self.cdh.saveContext()
-    }
 
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(application: UIApplication) {
